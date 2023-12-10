278e717efa6408cf9e690d69f297fbdde7599fdb
Luc Sinet <luc.sinet@gmail.com>
2023-12-10 21:24:18 +0100

    chore: format

--- test/ini.cpp
+++ test/ini.cpp
@@ -1,20 +1,24 @@
+#include "src/ini.h"
+
 #include <catch2/catch_test_macros.hpp>
-
 #include <sstream>
-#include "ini.h"
 
-SCENARIO("Reading an ini content from a stream", "[ini]") {
-  GIVEN("A person represented as one section and two attributes") {
+SCENARIO("Reading an ini content from a stream", "[ini]")
+{
+  GIVEN("A person represented as one section and two attributes")
+  {
     std::stringstream file;
 
     file << "[Person]\n";
     file << "name = Bob\n";
     file << "age = 40\n";
 
-    WHEN("The content is parsed") {
+    WHEN("The content is parsed")
+    {
       auto result = ini::read(file);
 
-      THEN("The Person data structure is correctly parsed") {
+      THEN("The Person data structure is correctly parsed")
+      {
         REQUIRE(result.has_value());
         REQUIRE(result.value().contains("Person"));
         REQUIRE(result.value()["Person"]["name"] == "Bob");
